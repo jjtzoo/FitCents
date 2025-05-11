f5c68481f1f445a65193f7b7c7a6d2918c04e6dd
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-12 01:49:45 +0800

    feat(js): finishing setting up code for server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-const connect = require('./connect')
+const connect = require('./connect');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000
+
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    connect.connectToServer();
+    console.log(`Servering is running on port ${PORT}`);
+});
