872f321342da8078a3a9ba10930cc58eba660ec5
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-13 14:36:12 +0800

    feat(server.js): connected userDataRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const connect = require("./connect");
 const express = require("express");
 const cors = require("cors");
 const recipes = require("./recipesRoutes");
+const userData = require("./recipesRoutes")
 
 const app = express();
 const PORT = 3001;
@@ -10,6 +11,7 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 app.use(recipes);
+app.use(userData);
 
 app.listen(PORT, () => {
     connect.connectToServer();
