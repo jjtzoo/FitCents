17235e1872987e00f0ee1469d98305d027408325
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-15 15:45:20 +0800

    refactor: migrate recipesRoute CRUD operations to use Mongoose models

--- backend/route/recipesRoute.js
+++ backend/route/recipesRoute.js
@@ -1,156 +1,106 @@
 const express = require("express");
-const database = require("./connect");
-const { ObjectId } = require("mongodb")
+const recipes = require("../models/recipe");
+const { route } = require("./recipesRoute");
 
-let recipesRoute = express.Router();
+
+let router = express.Router();
 
 // #1 Retrieve All
 // http://localhost:3001/recipes
-recipesRoute.route("/recipes").get(async (request, response) => {
-    let db = database.getDb();
-    let data = await db.collection("recipes").find({}).toArray();
-    console.log("Retrieve data", data);
-    if (data.length > 0) {
-        return response.json(data);
-    } else {
-        throw new Error("Data was not found.");
-    }
-});
-
-
-// #2 Retrieve One 
-recipesRoute.route("/recipes/:id").get(async (request, response) => {
-    let db = database.getDb();
-    let data = await db.collection("recipes").findOne({_id: request.params.id});
-    if (data) {
-        return response.json(data);
-    } else {
-        throw new Error("Data was not found.");
-    }
-});
-
-
-// #3 Create One
-recipesRoute.route("/recipes").post(async (request, response) => {
+router.get('/', async(req, res) => {
     try {
-        const db = database.getDb();
-
-        const {
-            _id,
-            meatPartId,
-            label,
-            totalMealCost,
-            caloriesPerServing,
-            ingredients
-        } = request.body;
-
-        if (!_id) {
-            return response.status(400).json({ error: "_id is required"});
+        const recipes = await recipes.find();
+        if (recipes.length > 0) {
+            return res.status(200).json({
+                message: 'All recipes record retrieve!',
+                recipes: recipes
+            });
+        } else {
+            return res.status(404).json({
+                error: "No records found."
+            });
         }
-
-        const mongoObject = {
-            _id,
-            meatPartId,
-            label,
-            totalMealCost,
-            caloriesPerServing,
-            ingredients: ingredients.map(item => ({
-                ingredient : item.ingredient,
-                quantity: item.quantity,
-                unit: item.unit,
-                pricePerUnit: item.pricePerUnit,
-                calories: item.calories,
-                cost: item.cost
-            }))
-
-        }
-    
-        const data = await db.collection("recipes").updateOne(mongoObject);
-        return response.status(201).json({
-            message: "New Recipe added successfully",
-            data: data
+    } catch (err) {
+        console.log("Error: ", err);
+        return res.status(500).json({
+            error: 'Internal Server Error.'
         })
-    } catch(error) {
-        if (error.code === 11000) {
-            return response.status(409).json({ error: "A recipe with that _id already exists"});
-        }
-        
-        return response.status(500).json({
-            error: "Something went wrong",
-            details: error.message
-        });
     }
-
 });
 
-// #4 Update One
-recipesRoute.route("/recipes/:id").put(async (request, response) => {
+// get one 
+route.get('/:id', async (req, res) => {
     try {
-        const db = database.getDb();
-        const {
-            meatPartId,
-            label,
-            totalMealCost,
-            caloriesPerServing,
-            ingredients
-        } = request.body;
-
-        const mongoObject = {
-            $set : {
-                meatPartId,
-                label,
-                totalMealCost,
-                caloriesPerServing,
-                ingredients: ingredients.map(item => ({
-                    ingredient : item.ingredient,
-                    quantity: item.quantity,
-                    unit: item.unit,
-                    pricePerUnit: item.pricePerUnit,
-                    calories: item.calories,
-                    cost: item.cost
-                }))
-            }
-        }
-    
-        const data = await db.collection("recipes").updateOne({_id: request.params.id}, mongoObject);
-
-        if(data.matchCount === 0) {
-            return response.status(404).json({
-                error: "User not found"
+        const recipe = await recipes.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({
+                error: "No records found."
             });
         }
 
-        return response.status(201).json({
-            message: "Recipe has been sucessfully updated",
-            data: data
+        return res.status(200).json({
+            message: "Recipe retrieved successfully!",
+            recipes : recipe
         })
-    } catch(error) {
-        console.error("An expected error occurred", error);
-        return response.status(500).json({ error: "Something went wrong on the server"});
-    }
-
-});
-
-// #5 Delete One
-
-recipesRoute.route("/recipes/:id").delete(async (request, response) => {
-    try {
-        const db = database.getDb();
-        const data = await db.collection("recipes").deleteOne({_id: request.params.id});
-
-        if (data.deletedCount === 0) {
-            return response.status(404).json({ error : "Recipe not found"});
-        }
-        
-        return response.status(200).json({
-            message: "Recipe successfully deleted",
-            data: data
+    } catch(err) {
+        console.log("Error:", err);
+        return res.status(500).json({
+            error: "Internal Server Error."
         });
-    } catch(error) {
-        console.error("An expected error occurred", error);
-        return response.status(500).json({ error: "Something went wrong on the server"});
     }
-
 });
 
-module.exports = recipesRoute;
+// createone
+route.post('/', async (req, res) => {
+    try {
+        const newRecipe = new recipes(req.body);
+        const savedRecipe = await newRecipe.save();
+
+        res.status(201).json({
+            message: "New user successfully created.",
+            newrecipe : savedRecipe
+        });
+    } catch(err) {
+        console.log("Error:", err);
+        return res.status(500).json({
+            error: "Internal Server Error."
+        });
+    }
+});
+
+// updateone
+route.put('/:id', async (req, res) => {
+    try {
+        const updateRecipe = await recipes.findByIdAndUpdate(req.params.id, req.body, {new : true});
+        if (!updateRecipe) {
+            return res.status(404).json({error: "No recipe found."})
+        }
+    } catch(err) {
+        console.log("Error:", err);
+        return res.status(500).json({
+            error: "Internal Server Error."
+        });
+    }
+});
+
+// deleteOne
+router.delete("/:d", async(req, res) => {
+    try {
+        const deleteRecipe = await recipes.findByIdAndDelete(req.params.id);
+         if (!deleteUser) {
+            return res.status(404).json({
+                error: "Recipe not found."
+            })
+        }
+        res.status(200).json({
+            message: "Recipe successfully deleted."
+        })
+    } catch(err) {
+        console.log("Error: ", err);
+        return response.status(500).json({
+            error: "Internal Server Error."
+        })
+    }
+});
+
+module.exports = router;
--- backend/route/userDataRoute.js
+++ backend/route/userDataRoute.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const router = express.Router();
 const userData = require("../models/userData");
 
