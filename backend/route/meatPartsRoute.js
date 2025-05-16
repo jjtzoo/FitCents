7cdf92a8bf9ce6f77b2446330fd8deb1d8b63fd8
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-16 22:46:03 +0800

    refactor: move meatParts route logic to new meatPartsController

--- backend/route/meatPartsRoute.js
+++ backend/route/meatPartsRoute.js
@@ -1,108 +1,22 @@
-const express = require("express")
-const meatParts = require("../models/meatParts")
+const express = require("express");
+const meatPartsController = require("../controllers/meatPartsController");
 
 router = express.Router()
 
 // #1 Retrieve All
 // http://localhost:3001/meatParts
-router.get("/", async(req, res) => {
-    try{
-        const data = await meatParts.find();
-        if(data.length > 0) {
-            return res.status(200).json({
-                message: 'All meat parts record retrieve!',
-                data: data
-            });
-        } else {
-            return res.status(404).json({
-                error: "No records found."
-            });
-        }
-    } catch (err) {
-        console.log("Error: ", err);
-        return res.status(500).json({
-            error: 'Internal Server Error.'
-        })
-    }
-});
+router.get("/", meatPartsController.getAllMeatPart);
 
 // Retrieve One
-router.get("/:id", async(req, res) => {
-    try{
-        const data = await meatParts.findById(req.params.id);
-        if (!data) {
-            return res.status(404).json({
-                error: "No records found."
-            });
-        }
-        return res.status(200).json({
-            message: "Recipe retrieved successfully!",
-            data : data
-        })
-    } catch (err) {
-        console.log("Error: ", err);
-        return res.status(500).json({
-            error: 'Internal Server Error.'
-        })
-    }
-});
+router.get("/:id", meatPartsController.getMeatPart);
 
 // Create One
-router.post("/", async(req, res) => {
-    try{
-        const data = new meatParts(req.body);
-        const savedData = await data.save();
-
-        res.status(201).json({
-            message: "New user successfully created.",
-            data : savedData
-        });
-    } catch (err) {
-        console.log("Error: ", err);
-        return res.status(500).json({
-            error: 'Internal Server Error.'
-        })
-    }
-});
+router.post("/", meatPartsController.createMeatPart);
 
 // Update One
-router.put("/:id", async(req, res) => {
-    try{
-        const data = await meatParts.findByIdAndUpdate(req.params.id, req.body, {new : true} );
-        if (!data) {
-            return res.status(404).json({error: 'No Meat-part found.'});
-        }
-        return res.status(200).json({
-            message: "User update successful",
-            data: data
-        });
-    } catch (err) {
-        console.log("Error: ", err);
-        return res.status(500).json({
-            error: 'Internal Server Error.'
-        })
-    }
-});
+router.put("/:id", meatPartsController.updateMeatParts);
 
 // Delete One
-router.delete("/:id", async(req, res) => {
-    try{
-        const data = await meatParts.findByIdAndUpdate(req.params.id);
-        if (!data) {
-            return res.status(404).json({
-                error: "Recipe not found."
-            })
-        }
-        res.status(200).json({
-            message: "Recipe successfully deleted.",
-            data: data
-        })
-    } catch (err) {
-        console.log("Error: ", err);
-        return res.status(500).json({
-            error: 'Internal Server Error.'
-        })
-    }
-});
+router.delete("/:id", meatPartsController.deleteMeatPart);
 
 module.exports = router;
