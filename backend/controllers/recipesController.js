bc2337d26f20eeef3c7820005a0d71c782f60380
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-16 15:27:34 +0800

    refactor: extract logic from recipesRoute to new recipesController

---
+++ backend/controllers/recipesController.js
@@ -0,0 +1,98 @@
+const Recipes = require("../models/recipe");
+
+exports.getAllRecipe = async(req, res) => {
+    try {
+        const recipes = await Recipes.find();
+        if (recipes.length > 0) {
+            return res.status(200).json({
+                message: 'All Recipes record retrieve!',
+                recipes
+            });
+        } else {
+            return res.status(404).json({
+                error: "No records found."
+            });
+        }
+    } catch (err) {
+        console.log("Error: ", err);
+        return res.status(500).json({
+            error: 'Internal Server Error.'
+        });
+    }
+};
+
+
+exports.getOneRecipe = async(req, res) => {
+    try {
+        const recipe = await Recipes.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({
+                error: "No records found."
+            });
+        }
+
+        return res.status(200).json({
+            message: "Recipe retrieved successfully!",
+            recipe
+        });
+    } catch(err) {
+        console.log("Error:", err);
+        return res.status(500).json({
+            error: "Internal Server Error."
+        });
+    }
+}
+
+exports.createRecipe = async(req, res) => {
+    try {
+        const newRecipe = new Recipes(req.body);
+        const savedRecipe = await newRecipe.save();
+
+        res.status(201).json({
+            message: "New recipe successfully created.",
+            savedRecipe
+        });
+    } catch(err) {
+        console.log("Error:", err);
+        return res.status(500).json({
+            error: "Internal Server Error."
+        });
+    }
+}
+
+exports.updateRecipe = async(req, res) => {
+    try {
+        const updateRecipe = await Recipes.findByIdAndUpdate(req.params.id, req.body, {new : true});
+        if (!updateRecipe) {
+            return res.status(404).json({error: "No recipe found."});
+        }
+        return res.status(200).json({
+            message: "Recipe updated successfully.",
+            updateRecipe
+        });
+    } catch(err) {
+        console.log("Error:", err);
+        return res.status(500).json({
+            error: "Internal Server Error."
+        });
+    }
+}
+
+exports.deleteRecipe = async(req, res) => {
+    try {
+        const deleteRecipe = await recipes.findByIdAndDelete(req.params.id);
+        if (!deleteRecipe) {
+            return res.status(404).json({
+                error: "Recipe not found."
+            });
+        }
+        res.status(200).json({
+            message: "Recipe successfully deleted."
+        });
+    } catch(err) {
+        console.log("Error: ", err);
+        return response.status(500).json({
+            error: "Internal Server Error."
+        });
+    }
+}
--- backend/route/recipesRoute.js
+++ backend/route/recipesRoute.js
@@ -1,109 +1,23 @@
 const express = require("express");
-const recipes = require("../models/recipe");
+const recipesController = require("../controllers/recipesController");
 
 
 let router = express.Router();
 
 // #1 Retrieve All
 // http://localhost:3001/recipes
-router.get('/', async(req, res) => {
-    try {
-        const recipes = await recipes.find();
-        if (recipes.length > 0) {
-            return res.status(200).json({
-                message: 'All recipes record retrieve!',
-                recipes
-            });
-        } else {
-            return res.status(404).json({
-                error: "No records found."
-            });
-        }
-    } catch (err) {
-        console.log("Error: ", err);
-        return res.status(500).json({
-            error: 'Internal Server Error.'
-        })
-    }
-});
+router.get('/', recipesController.getAllRecipe);
 
 // get one 
-router.get('/:id', async (req, res) => {
-    try {
-        const recipe = await recipes.findById(req.params.id);
-        if (!recipe) {
-            return res.status(404).json({
-                error: "No records found."
-            });
-        }
-
-        return res.status(200).json({
-            message: "Recipe retrieved successfully!",
-            recipe
-        })
-    } catch(err) {
-        console.log("Error:", err);
-        return res.status(500).json({
-            error: "Internal Server Error."
-        });
-    }
-});
+router.get('/:id', recipesController.getOneRecipe);
 
 // createone
-router.post('/', async (req, res) => {
-    try {
-        const newRecipe = new recipes(req.body);
-        const savedRecipe = await newRecipe.save();
-
-        res.status(201).json({
-            message: "New user successfully created.",
-            savedRecipe
-        });
-    } catch(err) {
-        console.log("Error:", err);
-        return res.status(500).json({
-            error: "Internal Server Error."
-        });
-    }
-});
+router.post('/', recipesController.createRecipe);
 
 // updateone
-router.put('/:id', async (req, res) => {
-    try {
-        const updateRecipe = await recipes.findByIdAndUpdate(req.params.id, req.body, {new : true});
-        if (!updateRecipe) {
-            return res.status(404).json({error: "No recipe found."})
-        }
-        return res.status(200).json({
-            message: "User update successful",
-            updateRecipe
-        })
-    } catch(err) {
-        console.log("Error:", err);
-        return res.status(500).json({
-            error: "Internal Server Error."
-        });
-    }
-});
+router.put('/:id', recipesController.updateRecipe);
 
 // deleteOne
-router.delete("/:d", async(req, res) => {
-    try {
-        const deleteRecipe = await recipes.findByIdAndDelete(req.params.id);
-        if (!deleteRecipe) {
-            return res.status(404).json({
-                error: "Recipe not found."
-            })
-        }
-        res.status(200).json({
-            message: "Recipe successfully deleted."
-        })
-    } catch(err) {
-        console.log("Error: ", err);
-        return response.status(500).json({
-            error: "Internal Server Error."
-        })
-    }
-});
+router.delete("/:d", recipesController.deleteRecipe);
 
 module.exports = router;
