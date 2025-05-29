3395e771af2d69379b8de2f9541f45b6624d9eb0
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-29 12:39:51 +0800

    feat: configure routes using createBrowserRouter with MainLayout and role-based dashboards

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,9 +3,9 @@ import { createBrowserRouter } from "react-router";
 
 // Layouts
 import PublicLayout from "./layouts/PublicLayout";
-import RegularLayouts from "./layouts/RegularLayout";
-import PremiumLayouts from "./layouts/PremiumLayout";
-import DeveloperLayouts from "./layouts/DeveloperLayout";
+import RegularLayout from "./layouts/RegularLayout";
+import PremiumLayout from "./layouts/PremiumLayout";
+import DeveloperLayout from "./layouts/DeveloperLayout";
 
 // Pages
 import Home from "./pages/Home";
@@ -18,3 +18,68 @@ import PickMealPlan from "./pages/PickMealPlan";
 import MagicRecipe from "./pages/MagicRecipe";
 import RealTimePantry from "./pages/RealTimePantry";
 import UserData from "./pages/UserData";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element : <PublicLayout />,
+        children : [
+            {
+                path: '',
+                element : <Home />
+            },
+            {
+                path: 'aboutus',
+                element: <AboutUs />
+            },
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                Register: 'register',
+                element : <Register />
+            }
+        ]
+    },
+    {
+        path: '/regular',
+        element: <RegularLayout />,
+        children : [
+            {
+                path : 'user',
+                element : <UserData />
+            },
+            {
+                path : '/automealplan',
+                element : <AutoMealPlan />
+            },
+        ]
+    },
+    {
+        path: '/premium', 
+        element: <PremiumLayout />,
+        children : [
+            {
+                path: 'user',
+                element : <UserData />
+            },
+            {
+                path : 'automealplan',
+                element: <AutoMealPlan />
+            },
+            {
+                path: 'pickmealplan',
+                element: <PickMealPlan />
+            },
+            {
+                path: 'inventory',
+                element: <RealTimePantry />
+            }
+        ]
+    },
+    {
+        path: 'devs',
+        element : <DeveloperLayout />
+    }
+])
