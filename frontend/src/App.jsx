8725f539f4c51c841cd1bd3606be876de98e125f
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-05-31 06:54:15 +0800

    feat: integrate RouterProvider for client-side routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { UserProvider } from "./context/UserContext.jsx"
+import { RouterProvider } from "react-router";
+import router from "./router.jsx";
+import { UserProvider } from "./context/UserContext.jsx";
 
 const App = () => {
   return (
     <UserProvider>
       <h1>App</h1>
+      <RouterProvider router={router} /> 
     </UserProvider>
   )
 }
