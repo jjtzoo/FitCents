234be2dede9d019367f3d14d65bcf4569f68a61c
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-06-05 14:47:49 +0800

    feat(models): create User model with full schema definition

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true},
+    name : { type: String, required: true},
+    age:{type: Number, required: true},
+    gender: { type:String, required: true},
+    weight: {type: Number, required: true},
+    activityLevel: {type: String, required:true},
+    weightGoal: { type: Number, requried: true},
+    restrictions: { type: [String], default: []},
+    preferences: { type: [String], default: []},
+},
+{timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
