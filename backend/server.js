014574f00ee1738ebf7ee7294470755b8c49ebae
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-06-05 14:07:18 +0800

    refactor(server): remove invalid import calls from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,7 @@
 const connectDB = require("./mongoose");
 const express = require("express");
 const cors = require("cors");
-const recipes = require("./route/recipesRoute");
-const userData = require("./route/userDataRoute");
-const userGoals = require("./route/userGoalsRoute");
-const userMeals = require("./route/userMealsRoute");
-const meatParts = require("./route/meatPartsRoute");
+
 const dotenv = require("dotenv");
 dotenv.config({path: "./config.env"});
 
@@ -18,11 +14,6 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/recipes",recipes);
-app.use("/api/userData", userData);
-app.use("/api/userGoals", userGoals);
-app.use("/api/userMeals", userMeals);
-app.use("/api/meatParts", meatParts);
 
 app.get("/", (req, res) => {
     res.send("API is running. Use /recipes, /userData, etc.");
